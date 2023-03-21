1e858a90645f08a94463ce69ba01f59326299e9e
bart <bartkawa@proton.me>
2023-03-21 21:04:09 +0100

    add styling to flashcard thumnbail

--- bric-a-brac/src/components/FlashcardThumbnail.jsx
+++ bric-a-brac/src/components/FlashcardThumbnail.jsx
@@ -1,6 +1,17 @@
+import { CgHashtag } from "react-icons/cg";
 import useNavigation from "../hooks/use-navigation";
+import useFlashcards from "../hooks/use-flashcards";
+import classNames from "classnames";
+
 function FlashcardThumbnail({ flashcard }) {
     const { navigate } = useNavigation();
+    const { languageCategories } = useFlashcards();
+
+    const circleStyle = classNames("w-5 h-5 rounded-full", {
+        "bg-red-800": flashcard.status === "hard",
+        "bg-blue-800": flashcard.status === "new",
+        "bg-green-800": flashcard.status === "easy",
+    });
     return (
         <div
             id={flashcard.id}
@@ -8,10 +19,16 @@ function FlashcardThumbnail({ flashcard }) {
                 navigate(`/flashcard/${flashcard.id}`);
             }}
             key={flashcard.id}
-            className='text-zinc-200 bg-zinc-800 p-8 rounded-xl cursor-pointer shadow-xl hover:bg-zinc-700'
+            className='text-zinc-200 bg-zinc-800 p-4 rounded-xl cursor-pointer shadow-xl hover:bg-zinc-700'
         >
-            <div>{flashcard.category}</div>
-            <div>{flashcard.hashtag}</div>
+            <div className='flex justify-between mb-4'>
+                <div>{languageCategories.find((x) => x.name === flashcard.category).icon}</div>
+                <div className={circleStyle}></div>
+            </div>
+            <div className='flex gap-2 items-center'>
+                <CgHashtag size={20} />
+                <div>{flashcard.hashtag}</div>
+            </div>
         </div>
     );
 }
---
+++ bric-a-brac/src/pages/SessionPage.jsx
@@ -0,0 +1,4 @@
+function SessionPage() {
+    return <h1>SESSION</h1>;
+}
+export default SessionPage;
